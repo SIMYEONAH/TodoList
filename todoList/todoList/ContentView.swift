1cc0cae79a7b4ae0bf3792522ab4cbd9f83ae436
Yeonah Shim <simya0528@naver.com>
2025-01-19 21:17:49 +0900

    Add button

--- todoList/todoList/ContentView.swift
+++ todoList/todoList/ContentView.swift
@@ -6,16 +6,54 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query(sort: \ToDo.isCompleted) private var toDos: [ToDo]
+    
+    @State private var isAlertShowing = false
+    @State private var toDoTitle = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("안녕하세요")
+        NavigationStack {
+            List {
+                ForEach(toDos) { toDo in
+                    Button {
+                        toDo.isCompleted.toggle()
+                    } label: {
+                        Image(systemName: toDo.isCompleted ?  "checkmark.circle.fill" : "circle")
+                    }
+                    
+                    Text(toDo.title)
+                }
+                .onDelete(perform: deleteToDos)
+            }
+            .navigationTitle("To do App")
+            .toolbar {
+                Button {
+                    isAlertShowing.toggle()
+                } label: {
+                    Image(systemName: "plus.circle")
+                }
+            }
+            .alert("Add To Do", isPresented: $isAlertShowing) {
+                TextField("Enter ToDo", text: $toDoTitle)
+                
+                Button{
+                    modelContext.insert(ToDo(title: toDoTitle, isCompleted: false))
+                } label: {
+                    Text("Add")
+                }
+            }
         }
-        .padding()
+    }
+    
+    func deleteToDos(_ indexSet: IndexSet) {
+        for index in indexSet {
+            let toDo = toDos[index]
+            modelContext.delete(toDo)
+        }
     }
 }
 
