f5aa4193b0fe6b05a86ebc2f71cb97ec3d8c95f0
Yeonah Shim <simya0528@naver.com>
2025-01-19 17:41:55 +0900

    first commit

---
+++ todoList/todoList/todoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoListApp.swift
+//  todoList
+//
+//  Created by 심연아 on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct todoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ todoList/todoListTests/todoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  todoListTests.swift
+//  todoListTests
+//
+//  Created by 심연아 on 1/19/25.
+//
+
+import Testing
+@testable import todoList
+
+struct todoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
